27452b65a270357921f0c5dcaec165fc8b2853ac
Henrique Lima <henrique.lima.un@gmail.com>
2022-07-09 18:04:02 +0100

    started second file

---
+++ SQLQuery2.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM Housing..housing
+
+-- Alter SaleDate datatype to date
+ALTER TABLE Housing..housing
+ALTER COLUMN SaleDate date
+
+/* Populate property address data that is missing.
+Houses with the same ParceID have the same address. Which means
+that if we have property addresses that has null value with a 
+parcelID of X and another house contaning information about
+the property address and with the same parcelID X, we can copy 
+that property address and use it on the first house. */
