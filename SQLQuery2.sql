3f3d1868ee7ac4cd60983319b11eade443f49645
Henrique Lima <henrique.lima.un@gmail.com>
2022-07-11 19:16:29 +0100

    Started Case

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -11,3 +11,74 @@ that if we have property addresses that has null value with a
 parcelID of X and another house contaning information about
 the property address and with the same parcelID X, we can copy 
 that property address and use it on the first house. */
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM Housing..housing as a
+JOIN Housing..housing as b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+--SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM Housing..housing as a
+JOIN Housing..housing as b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL
+
+-- Breaking out Address into individual columns
+
+-- Add Address column
+
+ALTER TABLE Housing..housing
+ADD PropertySplitAddress NVARCHAR(255);
+
+UPDATE Housing..housing
+SET PropertySplitAddress  = SUBSTRING(PropertyAddress, 1,CHARINDEX(',', PropertyAddress)-1);
+
+-- Add city column
+
+ALTER TABLE Housing..housing
+ADD PropertySplitCity NVARCHAR(255);
+
+UPDATE Housing..housing
+SET PropertySplitCity  = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress));
+
+-- Using Parse name
+
+SELECT 
+PARSENAME(REPLACE(OwnerAddress, ',', '.'),3), 
+PARSENAME(REPLACE(OwnerAddress, ',', '.'),2),
+PARSENAME(REPLACE(OwnerAddress, ',', '.'),1)
+FROM housing..housing
+
+ALTER TABLE Housing..housing
+ADD OwnerSplitAddress NVARCHAR(255);
+
+UPDATE Housing..housing
+SET OwnerSplitAddress  = PARSENAME(REPLACE(OwnerAddress, ',', '.'),3);
+
+ALTER TABLE Housing..housing
+ADD OwnerSplitCity NVARCHAR(255);
+
+UPDATE Housing..housing
+SET OwnerSplitCity  = PARSENAME(REPLACE(OwnerAddress, ',', '.'),2);
+
+ALTER TABLE Housing..housing
+ADD OwnerSplitSate NVARCHAR(255);
+
+UPDATE Housing..housing
+SET OwnerSplitSate  = PARSENAME(REPLACE(OwnerAddress, ',', '.'),1);
+
+SELECT *
+FROM housing
+
+-- Change Y and N to Yes and No
+
+SELECT DISTINCT(SoldAsVacant), Count(SoldAsVacant)
+FROM housing
+GROUP BY SoldAsVacant 
+ORDER BY 2
+
